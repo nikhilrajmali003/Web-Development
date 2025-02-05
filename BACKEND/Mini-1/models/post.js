cbe33360ac9ed815111d3dc48e6439fae0838cb9
Nikhil Raj Mali <nikhilrajmali042@gmail.com>
2025-02-05 20:44:08 +0530

    update

--- BACKEND/Mini-1/models/post.js
+++ BACKEND/Mini-1/models/post.js
@@ -1,20 +1,30 @@
 const mongoose = require("mongoose");
 
+// Correct MongoDB connection string
+mongoose.connect("mongodb://127.0.0.1:27017/miniproject") 
+
+
 const postSchema = new mongoose.Schema({
-   user:{
-    type:mongoose.Schema.Types.ObjectId,
-    ref:"user"
-   },
-   date:{
-    type:Date,
-    default:Date.now
-   },
-   content:String,
-   likes:[{
-    type:mongoose.Schema.Types.ObjectId,ref:"user"
-   }]
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",  // Reference to User model
+        required: true
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    content: {
+        type: String,
+        required: true
+    },
+    likes: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"  // Reference to User model for likes
+    }]
 });
 
-module.exports= mongoose.model("post", postSchema);
+const Post = mongoose.model("Post", postSchema);
 
+module.exports = Post;
 
